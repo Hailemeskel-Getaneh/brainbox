d140f7a42f3953661c6d692614c3b95ebe4a6322
Hailemeskel-Getaneh <hailegetaneh1221@gmail.com>
2026-01-04 08:37:30 +0300

    feat(frontend): add landing page and connect to backend health check

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,7 @@
+import LandingPage from './components/LandingPage';
 
 function App() {
-
-  return (
-    <>
-      <h1 className='font-bold underline text-green-800 text-6xl '>Hello World .</h1>
-<h2 className='text-red-900 font-bold text-4xl'>This is react with vite, typescript and tailwindcss</h2>
-    </>
-  )
+  return <LandingPage />;
 }
 
-export default App
+export default App;
