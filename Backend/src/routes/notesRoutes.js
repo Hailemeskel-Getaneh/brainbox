839834a3d435994e4c395478d8e278326b19b53b
Hailemeskel-Getaneh <hailegetaneh1221@gmail.com>
2026-01-12 13:25:55 +0300

    feat(notes): Add GET /api/tags/suggestions route

--- Backend/src/routes/notesRoutes.js
+++ Backend/src/routes/notesRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getNotes, createNote, deleteNote, searchNotes, updateNote } from '../controllers/notesController.js';
+import { getNotes, createNote, deleteNote, searchNotes, updateNote, getTagsSuggestions } from '../controllers/notesController.js';
 import authMiddleware from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.get('/search/:term', authMiddleware, searchNotes);
+router.get('/tags/suggestions', authMiddleware, getTagsSuggestions);
 router.get('/:topicId', authMiddleware, getNotes);
 router.post('/:topicId', authMiddleware, createNote);
 router.put('/:id', authMiddleware, updateNote);
