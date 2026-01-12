33302f84e33f76b244286e4854cef7272571d37a
Hailemeskel-Getaneh <hailegetaneh1221@gmail.com>
2026-01-12 13:20:34 +0300

    feat(topics): Add PUT /api/topics/:id route

--- Backend/src/routes/topicsRoutes.js
+++ Backend/src/routes/topicsRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { getTopics, createTopic, deleteTopic } from '../controllers/topicsController.js';
+import { getTopics, createTopic, deleteTopic, updateTopic } from '../controllers/topicsController.js';
 import authMiddleware from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 router.get('/', authMiddleware, getTopics);
 router.post('/', authMiddleware, createTopic);
+router.put('/:id', authMiddleware, updateTopic);
 router.delete('/:id', authMiddleware, deleteTopic);
 
 export default router;
