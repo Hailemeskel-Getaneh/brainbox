39dc35c794983f1649b952cb12c6633dde2fc778
Hailemeskel-Getaneh <hailegetaneh1221@gmail.com>
2026-01-21 09:47:06 +0300

    refactor(backend): remove console.log from login function

--- Backend/src/controllers/userController.js
+++ Backend/src/controllers/userController.js
@@ -31,7 +31,6 @@ export const register = async (req, res) => {
 
 export const login = async (req, res) => {
     // Authenticate a user by checking their email and password, then issue a JWT token.
-    console.log('Attempting user login.');
     const { email, password } = req.body;
 
     try {
